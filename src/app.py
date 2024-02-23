becacd2fbf71bce4e76876546db5dae34f10fdae
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2024-02-23 19:30:37 +0000

    correcion en los metodos

--- src/app.py
+++ src/app.py
@@ -25,19 +25,39 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/members', methods=['GET'])
+@app.route('/members', methods=['GET', 'POST'])
 def handle_hello():
+    response_body = {}
+    # This is how you can use the Family datastructure by calling its methods
+    if request.method == 'GET':
+        members = jackson_family.get_all_members()
+        response_body["hello"] = "world"
+        response_body["family"] = members
+        return response_body, 200
+    if request.method == 'POST':
+        data = request.json  # Recibo los datos del front (body)
+        #  Ejecuto el método para agregar el member en la lista
+        results = jackson_family.add_member(data)
+        response_body['message'] = 'Miembro agregado'
+        response_body['results'] = results
+        return response_body, 200
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
 
-
-    return jsonify(response_body), 200
-
+@app.route('/members/<int:id>', methods=['GET', 'DELETE'])
+def handle_member(id):
+    if request.method == 'GET':
+        results = jackson_family.get_member(id)
+        if results == []:
+            response_body = {'message': 'No encontrado'}
+            return  response_body, 405
+        response_body = {'member': results}
+        return response_body, 200
+    if request.method == 'DELETE':
+        results = jackson_family.delete_member(id)
+        response_body = {'message': 'Eliminado',
+                         'results': results}
+        return response_body, 200
+    
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
