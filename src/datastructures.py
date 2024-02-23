345d55f5e4012e36187ce16454b6931754ba103f
Jose Farrera <76214819+Jose120296@users.noreply.github.com>
2024-02-23 19:41:54 +0000

    Agregada la estructura

--- src/datastructures.py
+++ src/datastructures.py
@@ -13,23 +13,44 @@ class FamilyStructure:
         self.last_name = last_name
 
         # example list of members
-        self._members = []
+        self._members = [{'first_name': 'John',
+                          'last_name': self.last_name,
+                          'age': 33,
+                          'lucky_numbers': [7, 13, 22],
+                          'id': self._generate_id()},
+                         {'first_name': 'Jane',
+                          'last_name': self.last_name,
+                          'age': 35,
+                          'id': self._generate_id(),
+                          'lucky_numbers': [10, 14, 3]},
+                         {'first_name': 'Jimmy',
+                          'last_name': self.last_name,
+                          'age': 5,
+                          'id': self._generate_id(),
+                          'lucky_numbers': [1]}]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
-    def _generateId(self):
+    def _generate_id(self):
         return randint(0, 99999999)
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        member['id'] = self._generate_id()
+        member['last_name'] = self.last_name
+        self._members.append(member)
+        return self._members
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
-
+        self._members = [item for item in self._members if id != item['id']]
+        return self._members
+       
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        # return a member with the given id
+        for member in self._members:
+            if member['id'] == id:
+                return member
+        result = [item for item in self._members if id == item['id']]
+        return result
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
